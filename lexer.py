c472c4af831f75be4838111ccd3cbcd9416039f8
Alex Gaynor <alex.gaynor@gmail.com>
2008-10-07 19:53:00 -0400

    initial work

---
+++ lexer.py
@@ -0,0 +1,135 @@
+from ply import lex
+
+reserved = {
+    'if': 'IF',
+    'else': 'ELSE',
+    'elif': 'ELIF',
+    'while': 'WHILE',
+    'for': 'FOR',
+    'in': 'IN',
+    'def': 'DEF',
+    'class': 'CLASS',
+}
+
+tokens = (
+    # objects of sorts
+    'NAME',
+    
+    'FLOAT',
+    'INTEGER',
+    'TRUE',
+    'FALSE',
+    'NONE',
+    
+    # operators
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'MODULO',
+    'NOT',
+    
+    # assignment
+    'EQUALS',
+    'PLUS_EQUALS',
+    'MINUS_EQUALS',
+    'TIMES_EQUALS',
+    'DIVIDE_EQUALS',
+    
+    # comparisons
+    'EQ',
+    'LT',
+    'GT',
+    'LE',
+    'GE',
+    'AND',
+    'OR',
+    
+    # syntax
+    'LPAREN',
+    'RPAREN',
+    'COMMENT',
+    'COLON',
+
+) + tuple(reserved.values())
+
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_MODULO = r'%'
+
+t_NOT = r'not'
+t_AND = r'and'
+t_OR = r'or'
+
+t_EQUALS = r'='
+t_PLUS_EQUALS = r'\+='
+t_MINUS_EQUALS = r'-='
+t_TIMES_EQUALS = r'\*='
+t_DIVIDE_EQUALS = r'/='
+
+t_EQ = r'=='
+t_LT = r'<'
+t_GT = r'>'
+t_LE = r'<='
+t_GE = r'>='
+
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_COLON = r':'
+
+def t_FLOAT(t):
+    r'\d*\.\d+'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print "Line %d: Number %s is too large!" % (t.lineno, t.value)
+        t.value = 0.0
+    return t
+
+def t_INTEGER(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print "Line %d: Number %s is too large!" % (t.lineno, t.value)
+        t.value = 0
+    return t
+
+def t_newline(t):
+    r'\n'
+    t.lexler.lineno += 1
+
+def t_TRUE(t):
+    r'True'
+    t.value = True
+    return t
+
+def t_FALSE(t):
+    r'False'
+    t.value = False
+    return t
+
+def t_NONE(t):
+    r'None'
+    t.value = None
+    return t
+
+def t_NAME(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'NAME')
+    return t
+
+def t_COMMENT(t):
+    r'\#.*'
+    pass
+
+def t_error(t):
+    print "Illegal charecter '%s'" % t.value[0]
+    t.lexer.skip(1)
+
+t_ignore = ' '
+
+lexer = lex.lex()
+
---
+++ parser.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import operator
+
+from ply import yacc
+
+from lexer import lexer, tokens
+
+precedence = (
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'TIMES', 'DIVIDE'),
+    ('right', 'UMINUS'),
+    ('right', 'UPLUS'),
+)
+
+BINARY_OPS = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.div,
+    '%': operator.mod
+}
+
+UNARY_OPS = {
+    '+': operator.pos,
+    '-': operator.neg,
+    'not': operator.not_
+}
+
+COMPARISON_OPS = {
+    '==': operator.eq,
+    '<': operator.lt,
+    '>': operator.gt,
+    '<=': operator.le,
+    '>=': operator.ge,
+    'and': operator.and_,
+    'or': operator.or_,
+}
+
+names = {}
+
+def p_statement_assign(t):
+    '''
+    statement : NAME EQUALS expression
+              | NAME PLUS_EQUALS expression
+              | NAME MINUS_EQUALS expression
+              | NAME TIMES_EQUALS expression
+              | NAME DIVIDE_EQUALS expression
+    '''
+    if t[2] == '=':
+        names[t[1]] = t[3]
+    elif t[2] == '+=':
+        names[t[1]] += t[3]
+    elif t[2] == '-=':
+        names[t[1]] -= t[3]
+    elif t[2] == '*=':
+        names[t[1]] *= t[3]
+    elif t[2] == '/=':
+        names[t[1]] /= t[3]
+
+def p_statement_expr(t):
+    '''
+    statement : expression
+    '''
+    t[0] = t[1]
+    print t[0]
+
+def p_expression_binop(t):
+    '''
+    expression : expression PLUS expression
+               | expression MINUS expression
+               | expression TIMES expression
+               | expression DIVIDE expression
+               | expression MODULO expression
+    '''
+    t[0] = BINARY_OPS[t[2]](t[1], t[3])
+
+def p_expression_unaryop(t):
+    '''
+    expression : MINUS expression %prec UMINUS
+               | PLUS expression %prec UPLUS
+               | NOT expression
+    '''
+    t[0] = UNARY_OPS[t[1]](t[2])
+
+def p_expression_group(t):
+    '''
+    expression : LPAREN expression RPAREN
+    '''
+    t[0] = t[2]
+
+def p_expression_compare(t):
+    '''
+    expression : expression EQ expression
+               | expression LT expression
+               | expression GT expression
+               | expression LE expression
+               | expression GE expression
+               | expression AND expression
+               | expression OR expression
+    '''
+    t[0] = COMPARISON_OPS[t[2]](t[1], t[3])
+
+def p_number(t):
+    '''
+    NUMBER : FLOAT
+           | INTEGER
+    '''
+    t[0] = t[1]
+
+def p_expression_value(t):
+    '''
+    expression : NUMBER
+               | TRUE
+               | FALSE
+               | NONE
+    '''
+    t[0] = t[1]
+
+def p_expression_name(t):
+    '''
+    expression : NAME
+    '''
+    t[0] = names[t[1]]
+    
+def p_error(t):
+    print "Syntax error at '%s'" % t.value
+
+parser = yacc.yacc()
+
+if __name__ == '__main__':
+    while True:
+        try:
+            s = raw_input('alex > ')
+        except EOFError:
+            print
+            break
+        except KeyboardInterrupt:
+            print
+            continue
+        if not s:
+            continue
+        parser.parse(s, lexer = lexer)
