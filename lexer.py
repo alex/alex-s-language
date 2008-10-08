2f37b82ddfc62010a0d1f19b75269e1718699442
Alex Gaynor <alex.gaynor@gmail.com>
2008-10-08 13:27:11 -0400

    lexer is now an object

--- lexer.py
+++ lexer.py
@@ -1,147 +1,148 @@
 from ply import lex
 
-reserved = {
-    'if': 'IF',
-    'else': 'ELSE',
-    'elif': 'ELIF',
-    'while': 'WHILE',
-    'for': 'FOR',
-    'in': 'IN',
-    'def': 'DEF',
-    'class': 'CLASS',
-}
-
-tokens = (
-    # objects of sorts
-    'NAME',
+class Lexer(object):
+    def build(self, **kwargs):
+        self.lexer = lex.lex(object=self, **kwargs)
     
-    'FLOAT',
-    'INTEGER',
-    'TRUE',
-    'FALSE',
-    'NONE',
+    reserved = {
+        'if': 'IF',
+        'else': 'ELSE',
+        'elif': 'ELIF',
+        'while': 'WHILE',
+        'for': 'FOR',
+        'in': 'IN',
+        'def': 'DEF',
+        'class': 'CLASS',
+    }
+    tokens = (
+        # objects of sorts
+        'NAME',
+        
+        'FLOAT',
+        'INTEGER',
+        'TRUE',
+        'FALSE',
+        'NONE',
+        
+        # operators
+        'PLUS',
+        'MINUS',
+        'TIMES',
+        'DIVIDE',
+        'MODULO',
+        'POWER',
+        'NOT',
+        
+        # assignment
+        'EQUALS',
+        'PLUS_EQUALS',
+        'MINUS_EQUALS',
+        'TIMES_EQUALS',
+        'DIVIDE_EQUALS',
+        
+        # comparisons
+        'EQ',
+        'LT',
+        'GT',
+        'LE',
+        'GE',
+        'AND',
+        'OR',
+        
+        # syntax
+        'LPAREN',
+        'RPAREN',
+        'COMMENT',
+        'COLON',
+        'COMMA',
+        'NEWLINE',
+    ) + tuple(reserved.values())
+
+    t_ignore = ' '
+
+    t_PLUS = r'\+'
+    t_MINUS = r'-'
+    t_TIMES = r'\*'
+    t_DIVIDE = r'/'
+    t_MODULO = r'%'
+    t_POWER = r'\*\*'
+
+    t_EQUALS = r'='
+    t_PLUS_EQUALS = r'\+='
+    t_MINUS_EQUALS = r'-='
+    t_TIMES_EQUALS = r'\*='
+    t_DIVIDE_EQUALS = r'/='
+
+    t_EQ = r'=='
+    t_LT = r'<'
+    t_GT = r'>'
+    t_LE = r'<='
+    t_GE = r'>='
+
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_COLON = r':'
     
-    # operators
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
-    'MODULO',
-    'POWER',
-    'NOT',
+    def t_AND(self, t):
+        r'and'
+        return t
+
+    def t_OR(self, t):
+        r'or'
+        return t
+
+    def t_NOT(self, t):
+        'not'
+        return t
+
+    def t_FLOAT(self, t):
+        r'\d*\.\d+'
+        try:
+            t.value = float(t.value)
+        except ValueError:
+            print "Line %d: Number %s is too large!" % (t.lineno, t.value)
+            t.value = 0.0
+        return t
+
+    def t_INTEGER(self, t):
+        r'\d+'
+        try:
+            t.value = int(t.value)
+        except ValueError:
+            print "Line %d: Number %s is too large!" % (t.lineno, t.value)
+            t.value = 0
+        return t
+
+    def t_newline(self, t):
+        r'\n'
+        t.lexer.lineno += 1
+        t.type = 'NEWLINE'
+
+    def t_TRUE(self, t):
+        r'True'
+        t.value = True
+        return t
+
+    def t_FALSE(self, t):
+        r'False'
+        t.value = False
+        return t
+
+    def t_NONE(self, t):
+        r'None'
+        t.value = None
+        return t
+
+    def t_NAME(self, t):
+        r'[a-zA-Z_][a-zA-Z_0-9]*'
+        t.type = reserved.get(t.value, 'NAME')
+        return t
+
+    def t_COMMENT(self, t):
+        r'\#.*'
+        pass
+
+    def t_error(self, t):
+        print "Illegal charecter '%s'" % t.value[0]
+        t.lexer.skip(1)
     
-    # assignment
-    'EQUALS',
-    'PLUS_EQUALS',
-    'MINUS_EQUALS',
-    'TIMES_EQUALS',
-    'DIVIDE_EQUALS',
-    
-    # comparisons
-    'EQ',
-    'LT',
-    'GT',
-    'LE',
-    'GE',
-    'AND',
-    'OR',
-    
-    # syntax
-    'LPAREN',
-    'RPAREN',
-    'COMMENT',
-    'COLON',
-    'COMMA',
-    'NEWLINE',
-) + tuple(reserved.values())
-
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_MODULO = r'%'
-t_POWER = r'\*\*'
-
-t_EQUALS = r'='
-t_PLUS_EQUALS = r'\+='
-t_MINUS_EQUALS = r'-='
-t_TIMES_EQUALS = r'\*='
-t_DIVIDE_EQUALS = r'/='
-
-t_EQ = r'=='
-t_LT = r'<'
-t_GT = r'>'
-t_LE = r'<='
-t_GE = r'>='
-
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_COLON = r':'
-
-def t_AND(t):
-    r'and'
-    return t
-
-def t_OR(t):
-    r'or'
-    return t
-
-def t_NOT(t):
-    'not'
-    return t
-
-def t_FLOAT(t):
-    r'\d*\.\d+'
-    try:
-        t.value = float(t.value)
-    except ValueError:
-        print "Line %d: Number %s is too large!" % (t.lineno, t.value)
-        t.value = 0.0
-    return t
-
-def t_INTEGER(t):
-    r'\d+'
-    try:
-        t.value = int(t.value)
-    except ValueError:
-        print "Line %d: Number %s is too large!" % (t.lineno, t.value)
-        t.value = 0
-    return t
-
-def t_newline(t):
-    r'\n'
-    t.lexer.lineno += 1
-    t.type = 'NEWLINE'
-
-def t_TRUE(t):
-    r'True'
-    t.value = True
-    return t
-
-def t_FALSE(t):
-    r'False'
-    t.value = False
-    return t
-
-def t_NONE(t):
-    r'None'
-    t.value = None
-    return t
-
-def t_NAME(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'NAME')
-    return t
-
-def t_COMMENT(t):
-    r'\#.*'
-    pass
-
-def t_error(t):
-    print "Illegal charecter '%s'" % t.value[0]
-    t.lexer.skip(1)
-
-t_ignore = ' '
-
-lexer = lex.lex()
-
