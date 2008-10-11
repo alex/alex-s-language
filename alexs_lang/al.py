d2e5a6d2a9c067d0e57d54f6a25c3de1e8c2c580
Alex Gaynor <alex.gaynor@gmail.com>
2008-10-11 15:00:47 -0400

    added a) a file to run tests, b) a really basic test, c) a file to execute arbitrary code

---
+++ alexs_lang/al.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import sys
+
+from alexs_lang.interpreter import Interpreter
+
+if __name__ == '__main__':
+    if sys.argv[1:]:
+        Interpreter(open(sys.argv[1])).execute()
---
+++ tests/runtests.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+from alexs_lang.interpreter import Interpreter
+
+def main():
+    import alexs_lang
+    directory = os.path.join(os.path.dirname(alexs_lang.__file__),'../tests/')
+    executable = os.path.join(os.path.dirname(alexs_lang.__file__), 'al.py')
+    failed = []
+    for f in (os.path.join(directory, x) for x in os.listdir(directory) if x.endswith('.al')):
+        p = subprocess.Popen([executable, f], stdout=subprocess.PIPE)
+        try:
+            assert p.stdout.read() == open("%sout" % f[:-2]).read()
+            print '.',
+        except AssertionError:
+            print 'F',
+            failed.append(os.path.basename(f[:-2]))
+    print
+    if failed:
+        print "The following tests failed:"
+        print '\n'.join(failed)
+    else:
+        print "All tests passed"
+
+if __name__ == '__main__':
+    main()
