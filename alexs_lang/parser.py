f7bb82990b368a87aea1ae4e487e9f68a204eb2f
Alex Gaynor <alex.gaynor@gmail.com>
2008-10-11 00:05:30 -0400

    made the parser be class based

--- alexs_lang/parser.py
+++ alexs_lang/parser.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import operator
-
 from ply import yacc
 
 from alexs_lang.lexer import Lexer
@@ -15,138 +13,110 @@ precedence = (
     ('left', 'POWER'),
 )
 
-BINARY_OPS = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.div,
-    '%': operator.mod,
-    '**': operator.pow,
-}
+class Parser(object):
+    def build(self, **kwargs):
+        self.lexer = Lexer()
+        self.tokens = lexer.tokens
+        self.parser = yacc.yacc(object=self)
+    
+    def parse(self, code):
+        n = ast.NodeList()
+        for line in code:
+            n.append(self.parser.parse(line, lexer=self.lexer))
+        return n
+    
+    def p_statement(self, t):
+        '''
+        statement : statement NEWLINE
+        '''
+        t[0] = t[1]
 
-UNARY_OPS = {
-    '+': operator.pos,
-    '-': operator.neg,
-    'not': operator.not_
-}
+    def p_statement_expr(self, t):
+        '''
+        statement : expression
+        '''
+        t[0] = t[1]
 
-COMPARISON_OPS = {
-    '==': operator.eq,
-    '<': operator.lt,
-    '>': operator.gt,
-    '<=': operator.le,
-    '>=': operator.ge,
-    'and': operator.and_,
-    'or': operator.or_,
-}
+    def p_expression(self, t):
+        '''
+        expression : NAME EQUALS expression
+                   | NAME PLUS_EQUALS expression
+                   | NAME MINUS_EQUALS expression
+                   | NAME TIMES_EQUALS expression
+                   | NAME DIVIDE_EQUALS expression
+        '''
+        t[0] = ast.Assignment(ast.Name(t[1]), t[3], t[2])
 
-names = {}
+    def p_expression_binop(self, t):
+        '''
+        expression : expression PLUS expression
+                   | expression MINUS expression
+                   | expression TIMES expression
+                   | expression DIVIDE expression
+                   | expression MODULO expression
+                   | expression POWER expression
+        '''
+        t[0] = ast.BinaryOperation(t[1], t[3], t[2])
 
-def p_statement(t):
-    '''
-    statement : statement NEWLINE
-    '''
-    t[0] = t[1]
+    def p_expression_unaryop(self, t):
+        '''
+        expression : MINUS expression %prec UMINUS
+                   | PLUS expression %prec UPLUS
+                   | NOT expression
+        '''
+        t[0] = ast.UnaryOperation(t[2], t[1])
 
-def p_statement_expr(t):
-    '''
-    statement : expression
-    '''
-    t[0] = t[1]
+    def p_expression_group(self, t):
+        '''
+        expression : LPAREN expression RPAREN
+        '''
+        t[0] = t[2]
 
-def p_expression(t):
-    '''
-    expression : NAME EQUALS expression
-               | NAME PLUS_EQUALS expression
-               | NAME MINUS_EQUALS expression
-               | NAME TIMES_EQUALS expression
-               | NAME DIVIDE_EQUALS expression
-    '''
-    t[0] = ast.Assignment(ast.Name(t[1]), t[3], t[2])
+    def p_expression_compare(self, t):
+        '''
+        expression : expression EQ expression
+                   | expression LT expression
+                   | expression GT expression
+                   | expression LE expression
+                   | expression GE expression
+                   | expression AND expression
+                   | expression OR expression
+        '''
+        t[0] = ast.Comparison(t[1], t[3], t[2])
 
-def p_expression_binop(t):
-    '''
-    expression : expression PLUS expression
-               | expression MINUS expression
-               | expression TIMES expression
-               | expression DIVIDE expression
-               | expression MODULO expression
-               | expression POWER expression
-    '''
-    t[0] = ast.BinaryOperation(t[1], t[3], t[2])
+    def p_expression_constant(self, t):
+        '''
+        expression : NUMBER
+                   | BOOL
+                   | none
+        '''
+        t[0] = t[1]
 
-def p_expression_unaryop(t):
-    '''
-    expression : MINUS expression %prec UMINUS
-               | PLUS expression %prec UPLUS
-               | NOT expression
-    '''
-    t[0] = ast.UnaryOperation(t[2], t[1])
+    def p_number(self, t):
+        '''
+        NUMBER : FLOAT
+               | INTEGER
+        '''
+        t[0] = ast.Number(t[1])
 
-def p_expression_group(t):
-    '''
-    expression : LPAREN expression RPAREN
-    '''
-    t[0] = t[2]
+    def p_bool(self, t):
+        '''
+        BOOL : TRUE
+             | FALSE
+        '''
+        t[0] = ast.Boolean(t[1])
 
-def p_expression_compare(t):
-    '''
-    expression : expression EQ expression
-               | expression LT expression
-               | expression GT expression
-               | expression LE expression
-               | expression GE expression
-               | expression AND expression
-               | expression OR expression
-    '''
-    t[0] = ast.Comparison(t[1], t[3], t[2])
+    def p_none(self, t):
+        '''
+        none : NONE
+        '''
+        t[0] = ast.NoneVal()
 
-def p_expression_constant(t):
-    '''
-    expression : NUMBER
-               | BOOL
-               | none
-    '''
-    t[0] = t[1]
+    def p_expression_name(self, t):
+        '''
+        expression : NAME
+        '''
+        t[0] = ast.Name(t[1])
 
-def p_number(t):
-    '''
-    NUMBER : FLOAT
-           | INTEGER
-    '''
-    t[0] = ast.Number(t[1])
-
-def p_bool(t):
-    '''
-    BOOL : TRUE
-         | FALSE
-    '''
-    t[0] = ast.Boolean(t[1])
-
-def p_none(t):
-    '''
-    none : NONE
-    '''
-    t[0] = ast.NoneVal()
-
-def p_expression_name(t):
-    '''
-    expression : NAME
-    '''
-    t[0] = ast.Name(t[1])
-
-def p_error(t):
-    print "Syntax error at '%s'" % t.value
-
-lexer = Lexer()
-tokens = lexer.tokens
-parser = yacc.yacc()
-
-def parse(code):
-    '''
-    code should be an iterable, where each line is a line of code
-    '''
-    n = ast.NodeList()
-    for line in code:
-        n.append(parser.parse(line, lexer=lexer))
-    return n
+    def p_error(self, t):
+        print "Syntax error at '%s'" % t.value
